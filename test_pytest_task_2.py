73b99322c21093a22b8a6d8cbbfcd1cd8ea9b8d5
Danil Shibetsky <medivh@MacBook-Pro-Danil.local>
2024-11-15 20:40:20 +0300

    Test to task 2

--- test_pytest_task_2.py
+++ test_pytest_task_2.py
@@ -1,1 +1,15 @@
-from task2 import
+from task2 import SavingsAccount
+
+acc_primer = SavingsAccount('Tom')
+
+def test_add():
+    transactions = (-600,-900)
+    for transaction in (transactions):
+        if transaction > 0:
+            acc_primer.deposit(transaction)
+        else:
+            acc_primer.withdraw(transaction)
+
+acc_primer.get_balance()
+
+acc_primer.apply_interest()
